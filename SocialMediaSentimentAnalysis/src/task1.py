e67fb981a475fdab6e53c61f7cec6d3933f64c80
Geoffrey <gcloud@uncc.edu>
2026-06-04 17:03:06 -0400

    Updated project structure

--- SocialMediaSentimentAnalysis/src/task1.py
+++ SocialMediaSentimentAnalysis/src/task1.py
@@ -8,7 +8,11 @@ spark = SparkSession.builder.appName("HashtagTrends").getOrCreate()
 posts_df = spark.read.option("header", True).csv("input/posts.csv")
 
 # TODO: Split the Hashtags column into individual hashtags and count the frequency of each hashtag and sort descending
-
+hashtag_counts = posts_df \
+    .withColumn("hashtag", explode(split(col("Hashtags"), ","))) \
+    .groupBy("hashtag") \
+    .count() \
+    .orderBy(col("count").desc())
 
 # Save result
 hashtag_counts.coalesce(1).write.mode("overwrite").csv("outputs/hashtag_trends.csv", header=True)
